51f442d8ef8bf7fc70efc8bd968bf9273ae861d2
Martin Norlin <martin.norlin@it-huset.se>
2025-10-28 15:23:39 +0100

    try to make screen work

--- stage1/99-uconsole-kernel/00-run.sh
+++ stage1/99-uconsole-kernel/00-run.sh
@@ -2,10 +2,9 @@
 
 echo "Installing custom kernel from .deb files..."
 
-install -v -o 0 -g 0 -m 644 /pi-gen/custom-kernel/*.deb ${ROOTFS_DIR}/tmp/
+install -v -o 0 -g 0 -m 644 files/*.deb ${ROOTFS_DIR}/tmp/
 
-on_chroot << 'CHROOT_EOF'
+on_chroot << 'EOF'
 dpkg -i /tmp/*.deb || apt-get -f install -y
 rm -f /tmp/*.deb
-CHROOT_EOF
 EOF
--- stage2/02-net-tweaks/01-run.sh
+++ stage2/02-net-tweaks/01-run.sh
@@ -4,21 +4,26 @@
 # radiating on 5GHz bands until the WLAN regulatory domain is set.
 # Unfortunately, this also blocks bluetooth, so we whitelist the known
 # on-board BT adapters here.
-
+echo A
 mkdir -p "${ROOTFS_DIR}/var/lib/systemd/rfkill/"
+echo B
 #           5                 miniuart 4      miniuart Zero   miniuart other  other
 for addr in 107d50c000.serial 3f215040.serial 20215040.serial fe215040.serial soc; do
 	echo 0 > "${ROOTFS_DIR}/var/lib/systemd/rfkill/platform-${addr}:bluetooth"
+        echo B1
 done
-
+echo C
 if [ -v WPA_COUNTRY ]; then
+        echo D
 	on_chroot <<- EOF
 		SUDO_USER="${FIRST_USER_NAME}" raspi-config nonint do_wifi_country "${WPA_COUNTRY}"
 	EOF
 elif [ -d "${ROOTFS_DIR}/var/lib/NetworkManager" ]; then
 	# NetworkManager unblocks all WLAN devices by default. Prevent that:
-	cat > "${ROOTFS_DIR}/var/lib/NetworkManager/NetworkManager.state" <<- EOF
+	echo E
+        cat > "${ROOTFS_DIR}/var/lib/NetworkManager/NetworkManager.state" <<- EOF
 		[main]
 		WirelessEnabled=false
 	EOF
 fi
+echo F
